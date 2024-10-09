eae4921208d707abe19683aa3a7d2da2661d5913
Ankit <ankitbasu10@gmail.com>
2024-10-09 18:21:06 +0530

    commit

---
+++ cpp assignments/day35.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        vector<int> a(n);
+        
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+        }
+        
+        int max_diff = max(a[0], x - a[n - 1]);
+        for (int i = 1; i < n; ++i) {
+            max_diff = max(max_diff, a[i] - a[i - 1]);
+        }
+        
+        cout << max_diff << endl;
+    }
+    
+    return 0;
+}
