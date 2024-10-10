1887ac95dc0fe2a3d725628a67f042a85f8ebbe7
Ankit <ankitbasu10@gmail.com>
2024-10-10 17:12:07 +0530

    commit

---
+++ cpp assignments/day36.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i) cin >> a[i];
+        if (n % 2 == 0) cout << max(a[0], a[1]) << endl;
+        else {
+            int result = a[0];
+            for (int i = 1; i < n; i += 2) result = max(result, a[i]);
+            cout << result << endl;
+        }
+    }
+    return 0;
+}
