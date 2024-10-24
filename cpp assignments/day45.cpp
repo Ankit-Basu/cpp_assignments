52a40712727f0a33fa9d87bf98c92ac3afad2064
Ankit <ankitbasu10@gmail.com>
2024-10-24 11:31:21 +0530

    commit

---
+++ cpp assignments/day45.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int value) {
+        data = value;
+        left = right = nullptr;
+    }
+};
+
+void inorderTraversal(Node* root) {
+    if (root == nullptr)
+        return;
+    inorderTraversal(root->left);
+    cout << root->data << " ";
+    inorderTraversal(root->right);
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    inorderTraversal(root);
+    return 0;
+}
+
+
+
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int value) {
+        data = value;
+        left = right = nullptr;
+    }
+};
+
+void postorderTraversal(Node* root) {
+    if (root == nullptr)
+        return;
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    cout << root->data << " ";
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    postorderTraversal(root);
+    return 0;
+}
