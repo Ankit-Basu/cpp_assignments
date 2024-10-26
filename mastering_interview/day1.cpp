6427535f4df7012c3594756d564c35c3f26f73d8
Ankit <ankitbasu10@gmail.com>
2024-10-26 22:02:12 +0530

    commit

---
+++ mastering_interview/day1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; ++i) cin >> arr[i];
+
+    int minElem = arr[0], maxElem = arr[0];
+    for (int i = 1; i < n; ++i) {
+        if (arr[i] < minElem) minElem = arr[i];
+        else if (arr[i] > maxElem) maxElem = arr[i];
+    }
+
+    cout << "Minimum element is: " << minElem << endl;
+    cout << "Maximum element is: " << maxElem << endl;
+    return 0;
+}
