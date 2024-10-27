54999cf514798157e09b3dbe8b67070da211243a
Ankit <ankitbasu10@gmail.com>
2024-10-27 21:56:55 +0530

    commit

---
+++ mastering_interview/day2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int minDifference(int arr[], int n, int m) {
+    if (m == 0 || n == 0) return 0;
+    sort(arr, arr + n);
+    if (n < m) return -1;
+    int minDiff = INT_MAX;
+    for (int i = 0; i + m - 1 < n; i++) {
+        int diff = arr[i + m - 1] - arr[i];
+        minDiff = min(minDiff, diff);
+    }
+    return minDiff;
+}
+
+int main() {
+    int arr[] = {7, 3, 2, 4, 9, 12, 56};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int m = 3;
+    cout << minDifference(arr, n, m) << endl;
+    return 0;
+}
