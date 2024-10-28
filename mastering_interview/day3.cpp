012ec0e604b0d1899d6a95a648a1da4c440cc38e
Ankit <ankitbasu10@gmail.com>
2024-10-28 18:28:56 +0530

    commit

---
+++ mastering_interview/day3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+bool containsDuplicate(vector<int>& nums) {
+    unordered_set<int> seen;
+    for (int num : nums) {
+        if (seen.count(num)) return true;
+        seen.insert(num);
+    }
+    return false;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) cin >> nums[i];
+    cout << (containsDuplicate(nums) ? "true" : "false") << endl;
+    return 0;
+}
