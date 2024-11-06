45f8ae1940184def4a75ae347d5679556bc62be2
Ankit <ankitbasu10@gmail.com>
2024-11-06 19:56:07 +0530

    commit

---
+++ mastering_interview/day10.cpp
@@ -0,0 +1,52 @@
+Here is the C++ solution with `using namespace std` included:
+
+```cpp
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int>> findNeutralTriplets(vector<int>& positions) {
+    vector<vector<int>> result;
+    sort(positions.begin(), positions.end());
+
+    for (int i = 0; i < positions.size() - 2; ++i) {
+        if (i > 0 && positions[i] == positions[i - 1]) continue;
+
+        int left = i + 1;
+        int right = positions.size() - 1;
+
+        while (left < right) {
+            int sum = positions[i] + positions[left] + positions[right];
+            if (sum == 0) {
+                result.push_back({positions[i], positions[left], positions[right]});
+                while (left < right && positions[left] == positions[left + 1]) ++left;
+                while (left < right && positions[right] == positions[right - 1]) --right;
+                ++left;
+                --right;
+            } else if (sum < 0) {
+                ++left;
+            } else {
+                --right;
+            }
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    vector<int> positions = {-1, 0, 1, 2, -1, -4};
+    auto triplets = findNeutralTriplets(positions);
+
+    for (const auto& triplet : triplets) {
+        for (int position : triplet) {
+            cout << position << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+```
