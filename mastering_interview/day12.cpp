85e89ecd1b4c16e042f8e8e3e05ab44ed0a451a8
Ankit <ankitbasu10@gmail.com>
2024-11-14 18:53:46 +0530

    commit

---
+++ mastering_interview/day12.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+bool isAnagram(string s, string t) {
+    if (s.size() != t.size()) return false;
+    unordered_map<char, int> count;
+    for (char c : s) count[c]++;
+    for (char c : t) {
+        if (--count[c] < 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    string s1 = "anagram", t1 = "nagaram";
+    string s2 = "rat", t2 = "car";
+
+    cout << (isAnagram(s1, t1) ? "true" : "false") << endl;
+    cout << (isAnagram(s2, t2) ? "true" : "false") << endl;
+
+    return 0;
+}
