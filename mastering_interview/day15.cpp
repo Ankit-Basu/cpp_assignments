19aceac3ed360cce725bb83dfac84efec377e17d
Ankit <ankitbasu10@gmail.com>
2024-11-17 18:42:16 +0530

    commit

---
+++ mastering_interview/day15.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_set>
+#include <string>
+using namespace std;
+
+int lengthOfLongestSubstring(const string& s) {
+    unordered_set<char> seen;
+    int maxLength = 0, start = 0;
+
+    for (int end = 0; end < s.size(); ++end) {
+        while (seen.find(s[end]) != seen.end()) {
+            seen.erase(s[start]);
+            ++start;
+        }
+        seen.insert(s[end]);
+        maxLength = max(maxLength, end - start + 1);
+    }
+    return maxLength;
+}
+
+int main() {
+    string s;
+    cout << "Enter a string: ";
+    cin >> s;
+    cout << "Length of the longest substring without repeating characters: " 
+         << lengthOfLongestSubstring(s) << endl;
+    return 0;
+}
