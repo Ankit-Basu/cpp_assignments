5e2e7724e53d0f485f2122dc14193fd252838418
Ankit <ankitbasu10@gmail.com>
2024-11-18 19:24:40 +0530

    commit

--- cpp assignments/day47.cpp
+++ cpp assignments/day47.cpp
@@ -1,33 +1,21 @@
+//Question 1
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 int findIndex(const vector<int>& vec, int target) {
-    auto it = find(vec.begin(), vec.end(), target);
-    return it != vec.end() ? distance(vec.begin(), it) : -1;
-}
-
-vector<int> rotateVector(vector<int> vec, int k) {
-    k %= vec.size();
-    rotate(vec.rbegin(), vec.rbegin() + k, vec.rend());
-    return vec;
+    for (size_t i = 0; i < vec.size(); ++i) {
+        if (vec[i] == target) return i;
+    }
+    return -1;
 }
 
 int main() {
     vector<int> vec1 = {1, 3, 5, 7, 9};
     cout << findIndex(vec1, 7) << endl;
-    cout << findIndex(vec1, 10) << endl;
 
-    vector<int> vec2 = {1, 2, 3, 4, 5};
-    vec2 = rotateVector(vec2, 2);
-    for (int i : vec2) cout << i << " ";
-    cout << endl;
-
-    vector<int> vec3 = {10, 20, 30, 40};
-    vec3 = rotateVector(vec3, 1);
-    for (int i : vec3) cout << i << " ";
-    cout << endl;
+    vector<int> vec2 = {2, 4, 6, 8};
+    cout << findIndex(vec2, 10) << endl;
 
     return 0;
 }
