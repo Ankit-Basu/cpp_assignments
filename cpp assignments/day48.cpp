b47ebfedc344dc20ba750069e143e5975270065f
Ankit <ankitbasu10@gmail.com>
2024-11-19 19:20:32 +0530

    commit

--- cpp assignments/day48.cpp
+++ cpp assignments/day48.cpp
@@ -65,3 +65,38 @@ int main() {
 
     return 0;
 }
+
+
+//Question 3
+#include <iostream>
+#include <set>
+using namespace std;
+
+set<int> mergeSets(const set<int>& set1, const set<int>& set2) {
+    set<int> result(set1.begin(), set1.end());
+    result.insert(set2.begin(), set2.end());
+    return result;
+}
+
+int main() {
+    set<int> set1 = {1, 3, 5};
+    set<int> set2 = {2, 3, 4, 6};
+    set<int> result1 = mergeSets(set1, set2);
+
+    for (int num : result1) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    set1 = {7, 8};
+    set2 = {8, 9, 10};
+    set<int> result2 = mergeSets(set1, set2);
+
+    for (int num : result2) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
