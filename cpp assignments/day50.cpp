625e29f5adab8199412429c117221e493de721e8
Ankit <ankitbasu10@gmail.com>
2024-11-21 19:06:36 +0530

    commit

--- cpp assignments/day50.cpp
+++ cpp assignments/day50.cpp
@@ -28,3 +28,38 @@ int main() {
 
     return 0;
 }
+
+//Question 2
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+int findMin(Node* root) {
+    if (!root) return -1;
+    if (!root->left) return root->data;
+    return findMin(root->left);
+}
+
+int main() {
+    Node* root1 = new Node(20);
+    root1->left = new Node(10);
+    root1->right = new Node(30);
+    root1->left->left = new Node(5);
+    root1->left->right = new Node(15);
+
+    Node* root2 = new Node(50);
+    root2->left = new Node(40);
+    root2->left->left = new Node(30);
+
+    cout << findMin(root1) << endl;
+    cout << findMin(root2) << endl;
+
+    return 0;
+}
+
