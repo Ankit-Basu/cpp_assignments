cfaeef20e81a825a17aacd1e88bce96cba79c37c
Ankit <ankitbasu10@gmail.com>
2024-11-22 18:48:57 +0530

    commit

--- cpp assignments/day51.cpp
+++ cpp assignments/day51.cpp
@@ -33,3 +33,40 @@ int main() {
 
     return 0;
 }
+
+
+//Question 2
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+int closestValue(TreeNode* root, double target) {
+    int closest = root->val;
+    while (root) {
+        if (abs(root->val - target) < abs(closest - target))
+            closest = root->val;
+        root = target < root->val ? root->left : root->right;
+    }
+    return closest;
+}
+
+int main() {
+    TreeNode* root = new TreeNode(10);
+    root->left = new TreeNode(5);
+    root->right = new TreeNode(15);
+    root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(7);
+
+    cout << closestValue(root, 6) << endl;
+    cout << closestValue(root, 8) << endl;
+
+    return 0;
+}
+
