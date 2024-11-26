c9a2c62f4d8309c2348b59e09c027cecf483fbe7
Ankit <ankitbasu10@gmail.com>
2024-11-26 18:12:50 +0530

    commit

--- cpp assignments/day53.cpp
+++ cpp assignments/day53.cpp
@@ -27,3 +27,37 @@ int main() {
 
     return 0;
 }
+
+
+//Question 2
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+int main() {
+    int n, y, even_count = 0, odd_count = 0;
+    cin >> n;
+    vector<int> numbers(n);
+    for (int i = 0; i < n; i++) cin >> numbers[i];
+    cin >> y;
+
+    for (int num : numbers) {
+        if (num % 2 == 0) even_count++;
+        else odd_count++;
+    }
+
+    long long product = accumulate(numbers.begin(), numbers.end(), 1LL, [](long long a, int b) { return a * b; });
+
+    cout << even_count << endl;
+    cout << odd_count << endl;
+    cout << product << endl;
+    cout << (find(numbers.begin(), numbers.end(), y) != numbers.end() ? "Found" : "Not Found") << endl;
+
+    sort(numbers.rbegin(), numbers.rend());
+    for (int num : numbers) cout << num << " ";
+    cout << endl;
+
+    return 0;
+}
